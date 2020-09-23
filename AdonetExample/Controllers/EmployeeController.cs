7ed5a22c6488d8396256ef18e747a92f2f23b0de
yawar2019 <yawarali17@gmail.com>
2020-09-23 22:03:16 +0530

    comitted code edit and create

--- AdonetExample/Controllers/EmployeeController.cs
+++ AdonetExample/Controllers/EmployeeController.cs
@@ -15,5 +15,47 @@ namespace AdonetExample.Controllers
         {
             return View(db.GetEmployee());
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            int i = db.SaveEmployee(emp);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            int i = db.EditEmployee(emp);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
--- AdonetExample/Models/EmployeeDetail.cs
+++ AdonetExample/Models/EmployeeDetail.cs
@@ -21,12 +21,62 @@ namespace AdonetExample.Models
             foreach (DataRow dr in dt.Rows)
             {
                 EmployeeModel emp = new EmployeeModel();
-                emp.EmpId =Convert.ToInt32(dr[0]);
-                emp.EmpName =Convert.ToString(dr[1]);
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
                 emp.EmpSalary = Convert.ToInt32(dr[2]);
                 listobj.Add(emp);
             }
             return listobj;
         }
+        public int SaveEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("spr_insertEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+
+
+        public  EmployeeModel  GetEmployeeById(int? id)
+        {
+            
+            SqlCommand cmd = new SqlCommand("spr_getEmployeeDetailsbyId", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            EmployeeModel emp = new EmployeeModel();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                 
+            }
+            return emp;
+        }
+
+        
+
+             public int EditEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("spr_updateEmployeeDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@empid", emp.EmpId);
+            cmd.Parameters.AddWithValue("@empname", emp.EmpName);
+            cmd.Parameters.AddWithValue("@empsalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
     }
 }

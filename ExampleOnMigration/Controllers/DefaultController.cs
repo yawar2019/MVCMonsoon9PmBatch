d0379906d4e5c9e4878370ef33dbb2581cb4779f
yawar2019 <yawarali17@gmail.com>
2020-10-09 21:20:11 +0530

    Hyderabad

--- ExampleOnMigration/Controllers/DefaultController.cs
+++ ExampleOnMigration/Controllers/DefaultController.cs
@@ -10,6 +10,8 @@ namespace ExampleOnMigration.Controllers
     {
         // GET: Default
         EmployeeContext db = new EmployeeContext();
+        EmployeeEntities db1 = new EmployeeEntities();
+        
         public ActionResult Index()
         {
             return View(db.EmployeeModels.ToList());
@@ -18,6 +20,7 @@ namespace ExampleOnMigration.Controllers
         public ActionResult HtmlHelperExample()
         {
             EmployeeModel emp = new EmployeeModel();
+            ViewBag.Department = new SelectList(db1.Departments.ToList(),"DeptId", "DeptName",3);
             emp.Designation = "software developer";
             return View(emp);
         }
